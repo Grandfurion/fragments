da3f4c0ee3931efb51519da450e0efbeb739849d
Grandfurion <jarikpar@gmail.com>
2019-02-02 20:26:55 +0200

    123

---
+++ app/src/main/java/yaros/com/fragments/MainActivity.java
@@ -0,0 +1,17 @@
+package yaros.com.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, new MenuFragment(), MenuFragment.TAG);
+        }
+    }
+}
